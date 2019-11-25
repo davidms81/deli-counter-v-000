c2bc4a87270d5d3a3c775371be3d2b8e43a92673
David Schoenholtz <dmschoenholtz@gmail.com>
2019-11-25 21:13:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,13 @@
 # Write your code here.
 
 def line(katz_deli)
+  new_array = []
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    puts "The line is currently: "
+    katz_deli.each_with_index do |name, index|
+      new_array << "#{index + 1}: name, "
+    end
+    puts "The line is currently: 
   end
 end
